0c3c660f4c88d5c9781a73084bf378f3ab2492ae
eslam-abdelmoniem <eslam.abdelmoniem@vodafone.com>
2025-04-27 13:25:02 +0300

    add footer component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,11 @@
 import { HeaderComponent } from '@/shared/components';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
